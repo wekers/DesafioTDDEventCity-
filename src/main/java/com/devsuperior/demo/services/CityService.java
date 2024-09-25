e7f6c293b5580cb72b2142293182598e6b9d913d
wekers <fernando@wekers.org>
2024-09-25 20:06:50 -0300

    Test CityControllerIT 2- insertShouldInsertResource -> ok

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -6,6 +6,7 @@ import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,11 +17,20 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CityDTO> findAll() {
         List<City> list = repository.findAll(Sort.by("name"));
         return list.stream().map(CityDTO::new).collect(Collectors.toList());
     }
 
+
+    @Transactional
+    public CityDTO insert(CityDTO dto){
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
 
 
 }
